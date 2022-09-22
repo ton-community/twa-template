65f65e32106878a382b87f7eb8118df06d689b49
Shahar Yakir <shahar.yakir@gmail.com>
2022-09-22 21:20:21 +0300

    UI Fixes + Counter refresh on txn (#2)

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,26 +3,35 @@ import { useQuery } from "@tanstack/react-query";
 import { Address, toNano, beginCell } from "ton";
 import { Card } from "./Card";
 import { tc } from "./Ton-Connector";
-
+import { useState } from "react";
 
 export function Counter() {
   const connect = useTonhubConnect();
   const counterAddr = "EQC-QTihJV_B4f8M2nynateMLynaRT_uwNYnnuyy87kam-G7";
+  const [isIssuedTxn, setIssuedTxn] = useState(false);
 
-  const { isLoading, error, data, isFetching, fetchStatus, status, refetch } = useQuery(["counter"], async () => {
-    const { stack } = await tc.callGetMethod(
-      Address.parse(counterAddr),
-      "counter"
-    );
-    return BigInt(stack[0][1]).toString();
-  });
+  const { isLoading, error, data, isFetching, fetchStatus, status } = useQuery(
+    ["counter"],
+    async (): Promise<string> => {
+      const { stack } = await tc.callGetMethod(
+        Address.parse(counterAddr),
+        "counter"
+      );
+      const newData = BigInt(stack[0][1]).toString();
+      if (newData !== data) {
+        setIssuedTxn(false);
+      }
+      return newData;
+    },
+    { refetchInterval: isIssuedTxn ? 2000 : undefined }
+  );
 
   return (
     <Card title="Counter">
-      <h3>Counter number {isLoading ? "Loading..." : data}</h3>
+      <h3>Counter number {isFetching ? "Loading..." : data}</h3>
       <button
-        onClick={() => {
-          connect.api.requestTransaction({
+        onClick={async () => {
+          const txnStat = await connect.api.requestTransaction({
             to: counterAddr,
             value: toNano(0.01).toString(),
             payload: beginCell()
@@ -32,6 +41,10 @@ export function Counter() {
               .toBoc()
               .toString("base64"),
           });
+
+          if (txnStat.type === "success") {
+            setIssuedTxn(true);
+          }
         }}
       >
         Increment
